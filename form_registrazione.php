2da1326db03743304a93cdc81e9ca4ab7f8847b2
Luca Zanchetta <102416762+luca-zanchetta@users.noreply.github.com>
2022-05-07 09:41:45 +0200

    Fix adattamento grafiche negli avvisi

--- form_registrazione.php
+++ form_registrazione.php
@@ -23,153 +23,14 @@ if(isset($_POST['matricola']) && isset($_POST['nome']) && isset($_POST['cognome'
         ('{$_POST['matricola']}', '{$_POST['nome']}', '{$_POST['cognome']}', '{$pass_enc}');";
         try {
             if($resultQ = mysqli_query($mysqliConnection, $sqlInsert)) {
-                echo "
-                <?xml version=\"1.0\" encoding=\"UTF-8\"?>
-                <!DOCTYPE html
-                PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"
-                \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">
-
-                <html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">
-
-                <head>
-                    <title>Successo!</title>
-                    <link rel=\"stylesheet\" href=\"stileLogin.css\">
-                </head>
-
-                <body>
-                <form action=\"login.php\">
-                <div class=\"header\">
-                    <div class=\"nav-left\">
-                        <div class=\"nav-logo\">
-                            <a href=\"homepage.php\">
-                                <img src=\"https://store-images.s-microsoft.com/image/apps.51215.9007199266623456.05e3a154-d5ac-49d8-af6e-ab2f789dc26d.f443b25b-1668-48aa-8137-f8e5609aee45?mode=scale&q=90&h=300&w=300\" alt=\"logo\" width=\"90px\">
-                            </a>
-                        </div>
-                        <div class=\"vertical-bar\"></div>
-                            <h2>
-                                Infostud
-                            </h2>
-                    </div>
-                    <div class=\"nav-right\">
-                        <img src=\"https://w7.pngwing.com/pngs/73/580/png-transparent-arturia-business-logo-musical-instruments-individual-retirement-account-logo-business-sound.png\" alt=\"dasdas\" width=\"100px\">
-                    </div>
-                </div>
-                <div class=\"central-block\">
-                    <div class=\"scroll-root\">
-                        <div class=\"scroll-area\"> 
-                            <div class=\"body\">
-                                <div class=\"box\">
-                                    <h1>Inserimento avvenuto con successo!</h1>
-                                </div>
-                                <div>
-                                    <input class=\"bottoneHome\" type=\"submit\" name=\"invio\" value=\"Torna alla home\">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                </form>
-                </body>
-                </html>
-                ";
+                header('Location: avvisoOK.html');
             }
             else {
-                echo "
-                <?xml version=\"1.0\" encoding=\"UTF-8\"?>
-                <!DOCTYPE html
-                PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"
-                \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">
-
-                <html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">
-
-                <head>
-                    <title>ERRORE :(</title>
-                    <link rel=\"stylesheet\" href=\"stileLogin.css\">
-                </head>
-
-                <body>
-                <form action=\"login.php\">
-                <div class=\"header\">
-                    <div class=\"nav-left\">
-                        <div class=\"nav-logo\">
-                            <a href=\"homepage.php\">
-                                <img src=\"https://store-images.s-microsoft.com/image/apps.51215.9007199266623456.05e3a154-d5ac-49d8-af6e-ab2f789dc26d.f443b25b-1668-48aa-8137-f8e5609aee45?mode=scale&q=90&h=300&w=300\" alt=\"logo\" width=\"90px\">
-                            </a>
-                        </div>
-                        <div class=\"vertical-bar\"></div>
-                            <h2>
-                                Infostud
-                            </h2>
-                    </div>
-                    <div class=\"nav-right\">
-                        <img src=\"https://w7.pngwing.com/pngs/73/580/png-transparent-arturia-business-logo-musical-instruments-individual-retirement-account-logo-business-sound.png\" alt=\"dasdas\" width=\"100px\">
-                    </div>
-                </div>
-                <div class=\"central-block\">
-                    <div class=\"body\">
-                        <div class=\"box\">
-                            <h1>ERRORE: Inserimento fallito.</h1>
-                        </div>
-                        <div>
-                            <input class=\"bottoneHome\" type=\"submit\" name=\"invio\" value=\"Torna alla home\">
-                        </div>
-                    </div>
-                </div>
-
-                </form>
-                </body>
-                </html>
-                ";
+                header('Location: avvisoErrore.html');
             }
         }
         catch(mysqli_sql_exception $exception) {
-            echo "
-                <?xml version=\"1.0\" encoding=\"UTF-8\"?>
-                <!DOCTYPE html
-                PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"
-                \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">
-
-                <html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">
-
-                <head>
-                    <title>ERRORE :(</title>
-                    <link rel=\"stylesheet\" href=\"stileLogin.css\">
-                </head>
-
-                <body>
-                <form action=\"login.php\">
-                <div class=\"header\">
-                    <div class=\"nav-left\">
-                        <div class=\"nav-logo\">
-                            <a href=\"homepage.php\">
-                                <img src=\"https://store-images.s-microsoft.com/image/apps.51215.9007199266623456.05e3a154-d5ac-49d8-af6e-ab2f789dc26d.f443b25b-1668-48aa-8137-f8e5609aee45?mode=scale&q=90&h=300&w=300\" alt=\"logo\" width=\"90px\">
-                            </a>
-                        </div>
-                        <div class=\"vertical-bar\"></div>
-                            <h2>
-                                Infostud
-                            </h2>
-                    </div>
-                    <div class=\"nav-right\">
-                        <img src=\"https://w7.pngwing.com/pngs/73/580/png-transparent-arturia-business-logo-musical-instruments-individual-retirement-account-logo-business-sound.png\" alt=\"dasdas\" width=\"100px\">
-                    </div>
-                </div>
-                <div class=\"central-block\">
-                    <div class=\"body\">
-                        <div class=\"box\">
-                            <h1>ERRORE: Studente gi&agrave; presente nel db.</h1>
-                        </div>
-                        <div>
-                            <input class=\"bottoneHome\" type=\"submit\" name=\"invio\" value=\"Torna alla home\">
-                        </div>
-                    </div>
-                </div>
-
-                </form>
-                </body>
-                </html>
-                ";
+            header('Location: avvisoPresente.html');
         }
     }
     // Chiusura connessione
@@ -209,7 +70,7 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
     </div>
 </div>
 <div class="central-block"> 
-    <DIV class="body">
+    <div class="body">
         <h2 class="title">REGISTRATI</h2>   
 
             <form action="<?php $_SERVER['PHP_SELF']?>" method="post"> 
